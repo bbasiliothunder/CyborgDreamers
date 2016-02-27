68e31bcd5cd6b7ca3b8456f36948747a4e003e30
Shinryu <Shinryu>
2016-02-28 00:11:09 +0800

    added Splash

--- StateSubclass.h
+++ StateSubclass.h
@@ -33,9 +33,26 @@ private:
 public:
     MenuState(StateManager* sm);
     void handleInput(int u, int v);
-    void handleInput();
     void update(float dt);
     void draw(sf::RenderWindow& window) const;
+};
+
+class Splash: public State
+{
+private:
+    StateManager* sm;
+    bool isFadingIn;
+    bool isOnPause;
+    bool isFadingOut;
+    float timeCount;
+    int logoAlpha;
+public:
+    Splash(StateManager* sm);
+    void update(float dt);
+    void draw(sf::RenderWindow&) const;
+    void onActivate(std::string accept);
+    void onDeactivate();
+    void handleInput(int u, int v);
 };
 
 #endif // STATESUBCLASS_H_INCLUDED
