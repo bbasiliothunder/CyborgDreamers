c4908a8b524e142204eaef3112922261636e5b3e
Jino <breginald.basilio@yahoo.com>
2016-04-30 12:24:24 +0800

    Added some NPC function calls.

--- GameState.cpp
+++ GameState.cpp
@@ -42,9 +42,19 @@ void GameState::handleInput(int u, int v) {
 
 void GameState::update(float dt) {
     player.update(dt);
+    kid.update(dt, map, 0);
+    follower.update(dt, map, sf::Vector2f((float)(player.getIndexPosition().x * 32), (float)(player.getIndexPosition().y * 32)), player.getDirection(), &player);
+    tourist.update(dt, map, 1);
+    statue.update(dt, map, 2);
+    //std::cout << "Player X: " << player.getIndexPosition().x * 32 << std::endl;
+    //std::cout << "Player Y: " << player.getIndexPosition().y * 32<< std::endl;
 }
 
 void GameState::draw(sf::RenderWindow& window) const {
     map.draw(window);
     player.draw(window);
+    kid.draw(window);
+    follower.draw(window);
+    tourist.draw(window);
+    statue.draw(window);
 }
